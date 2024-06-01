33bc070c4dd41bc4edb5f769cb82666e04fd2ae0
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-01 15:25:16 +0200

    Implement BranchVehicleRepository.cs

---
+++ Backend/Car-Mender.Infrastructure/Repositories/BranchVehicleRepository.cs
@@ -0,0 +1,38 @@
+using Car_Mender.Domain.Entities;
+using Car_Mender.Domain.Repositories;
+using Car_Mender.Infrastructure.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Mender.Infrastructure.Repositories;
+
+public class BranchVehicleRepository(AppDbContext context) : IBranchVehicleRepository
+{
+    public async Task<BranchVehicle> GetBranchVehicleByIdAsync(Guid id)
+    {
+        return await context.Set<BranchVehicle>().FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IEnumerable<BranchVehicle>> GetAllBranchVehiclesAsync()
+    {
+        return await context.Set<BranchVehicle>().ToListAsync();
+    }
+
+    public async Task AddBranchVehicleAsync(BranchVehicle branchVehicle)
+    {
+        await context.Set<BranchVehicle>().AddAsync(branchVehicle);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateBranchVehicleAsync(BranchVehicle branchVehicle)
+    {
+        context.Set<BranchVehicle>().Update(branchVehicle);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteBranchVehicleAsync(Guid id)
+    {
+        var appointment = await GetBranchVehicleByIdAsync(id);
+        context.Set<BranchVehicle>().Remove(appointment);
+        await context.SaveChangesAsync();
+    }
+}
