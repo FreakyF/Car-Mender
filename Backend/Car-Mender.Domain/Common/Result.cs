cbd3414213be0926aa62578fe9b42f1f13ac2315
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-02 00:27:03 +0200

    Add non generic Result pattern

--- Backend/Car-Mender.Domain/Common/Result.cs
+++ Backend/Car-Mender.Domain/Common/Result.cs
@@ -20,7 +20,26 @@ public sealed class Result<T>
     public static implicit operator Result<T>(Error error) => Failure(error);
 }
 
+public class Result
+{
+    public Error Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+
+    private Result(bool success,Error error)
+    {
+        IsSuccess = success;
+        Error = error;
+    }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+    
+    public static implicit operator Result(Error error) => Failure(error);
+}
+
 public sealed record Error(string code, string? description = null)
 {
     public static readonly Error None = new(string.Empty);
+    public static readonly Error Unexpected = new("UNEXPECTED_ERROR", "An unexpected error occured");
 }
