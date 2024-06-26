af762e175b2185c753924b20f3eba2d5d7a43db6
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-26 20:10:33 +0200

    Format CompanyErrors.cs

--- Backend/Car-Mender.Domain/Features/Companies/Errors/CompanyErrors.cs
+++ Backend/Car-Mender.Domain/Features/Companies/Errors/CompanyErrors.cs
@@ -4,5 +4,6 @@ namespace Car_Mender.Domain.Features.Companies.Errors;
 
 public static class CompanyErrors
 {
-    public static readonly Error CouldNotBeFound = new(CompanyErrorCodes.CouldNotBeFound, "Given company could not be found.");
+	public static readonly Error CouldNotBeFound =
+		new(CompanyErrorCodes.CouldNotBeFound, "Given company could not be found.");
 }
