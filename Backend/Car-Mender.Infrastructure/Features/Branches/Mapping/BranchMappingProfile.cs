43472a1127495a0f83baeaf0a0bba4db24ec37dc
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-26 20:11:21 +0200

    Implement BranchMappingProfile.cs

---
+++ Backend/Car-Mender.Infrastructure/Features/Branches/Mapping/BranchMappingProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Car_Mender.Domain.Features.Branches.DTOs;
+using Car_Mender.Domain.Features.Branches.Entities;
+using Car_Mender.Infrastructure.Features.Branches.Commands.CreateBranch;
+
+namespace Car_Mender.Infrastructure.Features.Branches.Mapping;
+
+public class BranchMappingProfile : Profile
+{
+	public BranchMappingProfile()
+	{
+		CreateMap<CreateBranchCommand, Branch>()
+			.ForMember(dest => dest.Id, opt => opt.Ignore())
+			.ForMember(dest => dest.Company, opt => opt.Ignore());
+		CreateMap<Branch, GetBranchDto>();
+	}
+}
