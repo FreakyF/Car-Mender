55e2a8b90c8238907e52350b1607e60bf205cc86
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-26 21:48:42 +0200

    Implement checks for when company does not exists and it's result fails.

--- Backend/Car-Mender.Infrastructure/Features/Branches/Commands/CreateBranch/CreateBranchCommandHandler.cs
+++ Backend/Car-Mender.Infrastructure/Features/Branches/Commands/CreateBranch/CreateBranchCommandHandler.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using Car_Mender.Domain.Common;
 using Car_Mender.Domain.Features.Branches.Entities;
 using Car_Mender.Domain.Features.Branches.Repository;
+using Car_Mender.Domain.Features.Companies.Errors;
+using Car_Mender.Domain.Features.Companies.Repository;
 using FluentValidation;
 using MediatR;
 
@@ -9,12 +11,25 @@ namespace Car_Mender.Infrastructure.Features.Branches.Commands.CreateBranch;
 
 public class CreateBranchCommandHandler(
 	IBranchRepository branchRepository,
+	ICompanyRepository companyRepository,
 	IValidator<CreateBranchCommand> validator,
 	IMapper mapper
 ) : IRequestHandler<CreateBranchCommand, Result<Guid>>
 {
 	public async Task<Result<Guid>> Handle(CreateBranchCommand request, CancellationToken cancellationToken)
 	{
+		var companyExistsResult = await companyRepository.ExistsAsync(request.CompanyId);
+		if (companyExistsResult.IsFailure)
+		{
+			return Result<Guid>.Failure(companyExistsResult.Error);
+		}
+
+		var companyExists = companyExistsResult.Value;
+		if (!companyExists)
+		{
+			return CompanyErrors.CouldNotBeFound;
+		}
+
 		var validationResult = await validator.ValidateAsync(request, cancellationToken);
 		if (!validationResult.IsValid)
 		{
