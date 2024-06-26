1cb5c42c28a43dfd282ba130264ed911ab1189ee
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-26 21:44:43 +0200

    Implement HTTPPatch for BranchController.cs

--- Backend/Car-Mender.API/Features/Branches/BranchController.cs
+++ Backend/Car-Mender.API/Features/Branches/BranchController.cs
@@ -1,9 +1,12 @@
 using Car_Mender.Domain.Common;
 using Car_Mender.Domain.Features.Branches.DTOs;
 using Car_Mender.Domain.Features.Branches.Errors;
+using Car_Mender.Domain.Features.Companies.Errors;
 using Car_Mender.Infrastructure.Features.Branches.Commands.CreateBranch;
+using Car_Mender.Infrastructure.Features.Branches.Commands.UpdateBranch;
 using Car_Mender.Infrastructure.Features.Branches.Queries;
 using MediatR;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Car_Mender.API.Features.Branches;
@@ -44,6 +47,30 @@ public class BranchController(IMediator mediator) : ControllerBase
 			ErrorCodes.InvalidId => BadRequest(getBranchResult.Error.Description),
 			BranchErrorCodes.CouldNotBeFound => NotFound(getBranchResult.Error.Description),
 			_ => StatusCode(500)
+		};
+	}
+
+	[HttpPatch("{id:guid}")]
+	public async Task<IActionResult> UpdateBranchById(Guid id,
+		[FromBody] JsonPatchDocument<UpdateBranchDto> patchDocument)
+	{
+		if (patchDocument is null)
+		{
+			return BadRequest("Invalid Json Patch Document");
+		}
+
+		var command = new UpdateBranchCommand(id, patchDocument);
+		var updateBranchResult = await mediator.Send(command);
+		if (updateBranchResult.IsSuccess)
+		{
+			return NoContent();
+		}
+
+		return updateBranchResult.Error.Code switch
+		{
+			ErrorCodes.InvalidId => BadRequest(updateBranchResult.Error.Description),
+			CompanyErrorCodes.CouldNotBeFound => NotFound(updateBranchResult.Error.Description),
+			_ => StatusCode(500, updateBranchResult.Error.Description)
 		};
 	}
 }
