8873a25fe195b3be81a3b45472b53508cd60d156
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-26 21:42:15 +0200

    Create UpdateBranchCommandValidator.cs

---
+++ Backend/Car-Mender.Infrastructure/Features/Branches/Commands/UpdateBranch/UpdateBranchCommandValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+
+namespace Car_Mender.Infrastructure.Features.Branches.Commands.UpdateBranch;
+
+public class UpdateBranchCommandValidator : AbstractValidator<UpdateBranchCommand>
+{
+	public UpdateBranchCommandValidator()
+	{
+		// TODO: :)
+	}
+}
