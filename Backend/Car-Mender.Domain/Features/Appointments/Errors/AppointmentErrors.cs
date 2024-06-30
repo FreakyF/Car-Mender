3f2030158b7d7e94d8acae1d0f128512d8571b70
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-30 19:26:22 +0200

    Make AppointmentErrors a static class.

--- Backend/Car-Mender.Domain/Features/Appointments/Errors/AppointmentErrors.cs
+++ Backend/Car-Mender.Domain/Features/Appointments/Errors/AppointmentErrors.cs
@@ -2,7 +2,7 @@ using Car_Mender.Domain.Common;
 
 namespace Car_Mender.Domain.Features.Appointments.Errors;
 
-public class AppointmentErrors
+public static class AppointmentErrors
 {
 	public static readonly Error CouldNotBeFound =
 		new(AppointmentErrorCodes.CouldNotBeFound, "Given appointment could not be found.");
