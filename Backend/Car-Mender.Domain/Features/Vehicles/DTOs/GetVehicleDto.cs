5d5f8c2c56ac5e92139f2e05eeee6b3611a0f83e
Kamil Fudala <kamil.fudala44@gmail.com>
2024-06-30 16:34:36 +0200

    Create Dto for update and get methods for Vehicle.

--- Backend/Car-Mender.Domain/Features/Vehicles/DTOs/GetVehicleDto.cs
+++ Backend/Car-Mender.Domain/Features/Vehicles/DTOs/GetVehicleDto.cs
@@ -1,6 +1,13 @@
+using Car_Mender.Domain.Features.Engines.Entities;
+
 namespace Car_Mender.Domain.Features.Vehicles.DTOs;
 
 public class GetVehicleDto
 {
-	
+	public Guid EngineId { get; init; }
+	public required string Vin { get; init; }
+	public required string Make { get; init; }
+	public required string Model { get; init; }
+	public required string Generation { get; init; }
+	public required uint Year { get; init; }
 }
--- Backend/Car-Mender.Domain/Features/Vehicles/DTOs/UpdateVehicleDto.cs
+++ Backend/Car-Mender.Domain/Features/Vehicles/DTOs/UpdateVehicleDto.cs
@@ -2,5 +2,5 @@ namespace Car_Mender.Domain.Features.Vehicles.DTOs;
 
 public class UpdateVehicleDto
 {
-	
+	public Guid EngineId { get; init; }
 }
