9906665d6cc96d4d4791e65b771d7badaa3f02b4
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-01 20:52:52 +0200

    Create DeleteIssueCommand with its handler.

--- Backend/Car-Mender.Infrastructure/Features/Issues/Commands/DeleteIssue/DeleteIssueCommand.cs
+++ Backend/Car-Mender.Infrastructure/Features/Issues/Commands/DeleteIssue/DeleteIssueCommand.cs
@@ -1,6 +1,6 @@
+using Car_Mender.Domain.Common;
+using MediatR;
+
 namespace Car_Mender.Infrastructure.Features.Issues.Commands.DeleteIssue;
 
-public class DeleteIssueCommand
-{
-	
-}
+public record DeleteIssueCommand(Guid Id) : IRequest<Result>;
--- Backend/Car-Mender.Infrastructure/Features/Issues/Commands/DeleteIssue/DeleteIssueCommandHandler.cs
+++ Backend/Car-Mender.Infrastructure/Features/Issues/Commands/DeleteIssue/DeleteIssueCommandHandler.cs
@@ -1,6 +1,24 @@
+using Car_Mender.Domain.Common;
+using Car_Mender.Domain.Features.Issues.Repository;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
 namespace Car_Mender.Infrastructure.Features.Issues.Commands.DeleteIssue;
 
-public class DeleteIssueCommandHandler
+public class DeleteIssueCommandHandler(
+	IIssueRepository workerRepository,
+	ILogger<DeleteIssueCommandHandler> logger)
+	: IRequestHandler<DeleteIssueCommand, Result>
 {
-	
+	public async Task<Result> Handle(DeleteIssueCommand request, CancellationToken cancellationToken)
+	{
+		if (request.Id.Equals(Guid.Empty)) return Error.InvalidId;
+
+		var deleteIssueResult = await workerRepository.DeleteIssueAsync(request.Id, cancellationToken);
+		if (deleteIssueResult.IsFailure)
+			logger.LogError("Could not remove issue with id: {Id}: {Message}", request.Id,
+				deleteIssueResult.Error.Description);
+
+		return deleteIssueResult;
+	}
 }
