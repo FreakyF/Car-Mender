151ff2965c4a78c546c85e7fc78db95713a9f077
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-01 21:06:12 +0200

    Implement CreateIssueCommandValidator.cs validation rules.

--- Backend/Car-Mender.Infrastructure/Features/Issues/Commands/CreateIssue/CreateIssueCommandValidator.cs
+++ Backend/Car-Mender.Infrastructure/Features/Issues/Commands/CreateIssue/CreateIssueCommandValidator.cs
@@ -1,6 +1,28 @@
+using Car_Mender.Domain.Features.Issues.Entities;
+using FluentValidation;
+
 namespace Car_Mender.Infrastructure.Features.Issues.Commands.CreateIssue;
 
-public class CreateIssueCommandValidator
+public class CreateIssueCommandValidator : AbstractValidator<CreateIssueCommand>
 {
-	
+	public CreateIssueCommandValidator()
+	{
+		RuleFor(i => i.Description)
+			.NotEmpty().WithMessage($"{nameof(Issue.Description)} cannot be empty.")
+			.MaximumLength(1000)
+			.WithMessage($"{nameof(Issue.Description)} cannot be longer than 1000 characters.");
+
+		RuleFor(i => i.Status)
+			.IsInEnum().WithMessage(
+				$"{nameof(Issue.Status)}  must be a valid issue status:  {{string.Join(\", \", Enum.GetNames(typeof(IssueStatus)))}}.");
+
+		RuleFor(i => i.ReporterType)
+			.IsInEnum().WithMessage(
+				$"{nameof(Issue.Status)}  must be a valid reporter type:  {{string.Join(\", \", Enum.GetNames(typeof(ReporterType)))}}.");
+
+		RuleFor(i => i.ReportedDate)
+			.NotEmpty().WithMessage($"{nameof(Issue.ReportedDate)} cannot be empty.")
+			.Must(date => date.Date <= DateTime.Today)
+			.WithMessage($"{nameof(Issue.ReportedDate)} must be today or in the past.");
+	}
 }
