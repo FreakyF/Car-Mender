0ad5832e3c53e0911882268e40642e922269f82e
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-01 20:02:32 +0200

    Fix return type.

--- Backend/Car-Mender.Domain/Features/Workers/Repository/IWorkerRepository.cs
+++ Backend/Car-Mender.Domain/Features/Workers/Repository/IWorkerRepository.cs
@@ -7,7 +7,7 @@ public interface IWorkerRepository
 {
 	Task<Result<Worker>> GetWorkerByIdAsync(Guid id, CancellationToken cancellationToken);
 	Task<Result<Worker>> GetWorkerByIdNoTrackingAsync(Guid id, CancellationToken cancellationToken);
-	Task<IEnumerable<Worker>> GetAllWorkersAsync();
+	Task<Result<IEnumerable<Worker>>> GetAllWorkersAsync();
 	Task<Result<Guid>> CreateWorkerAsync(Worker worker, CancellationToken cancellationToken);
 	Task<Result> UpdateWorkerAsync(Worker worker, CancellationToken cancellationToken);
 	Task<Result> DeleteWorkerAsync(Guid id, CancellationToken cancellationToken);
