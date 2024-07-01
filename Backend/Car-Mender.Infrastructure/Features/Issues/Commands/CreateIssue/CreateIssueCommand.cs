7bbe2238ed4f0f2b52c28b9bbd39c45abf67f133
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-01 20:31:37 +0200

    Implement CreateIssueCommand.cs

--- Backend/Car-Mender.Infrastructure/Features/Issues/Commands/CreateIssue/CreateIssueCommand.cs
+++ Backend/Car-Mender.Infrastructure/Features/Issues/Commands/CreateIssue/CreateIssueCommand.cs
@@ -1,6 +1,16 @@
+using Car_Mender.Domain.Common;
+using Car_Mender.Domain.Enums;
+using MediatR;
+
 namespace Car_Mender.Infrastructure.Features.Issues.Commands.CreateIssue;
 
-public class CreateIssueCommand
+// ReSharper disable once ClassNeverInstantiated.Global
+public class CreateIssueCommand : IRequest<Result<Guid>>
 {
-	
+	public Guid AppointmentId { get; set; }
+	public Guid CreatorId { get; set; }
+	public required ReporterType ReporterType { get; set; }
+	public required string Description { get; set; }
+	public required DateTime ReportedDate { get; set; }
+	public required IssueStatus Status { get; set; }
 }
