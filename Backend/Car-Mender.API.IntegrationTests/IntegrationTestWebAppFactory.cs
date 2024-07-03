781d70484c24d43b813f5c6414dfa382df138eb5
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-03 22:24:39 +0200

    Remove unnecessary todo comments.

--- Backend/Car-Mender.API.IntegrationTests/IntegrationTestWebAppFactory.cs
+++ Backend/Car-Mender.API.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -13,12 +13,6 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
 		.WithImage("mcr.microsoft.com/mssql/server:2022-latest")
 		.WithPassword("Strong_password123!")
 		.Build();
-
-	// TODO: https://www.milanjovanovic.tech/blog/testcontainers-integration-testing-using-docker-in-dotnet
-	// TODO: https://www.youtube.com/watch?v=tj5ZCtvgXKY
-	// TODO: https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0
-	// TODO: Setup environment (WebappFActory, BaseIntegratonTest, TestContainers)
-	// TODO: Create tests for CompanyController - call API using TestServer's client. Check if you could create company. Expect successfull status code.
 	public Task InitializeAsync()
 	{
 		return _dbContainer.StartAsync();
