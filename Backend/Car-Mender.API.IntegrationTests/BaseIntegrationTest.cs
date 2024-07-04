ded08894165c7c053442b8129e8fc32d7d3ffeb2
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-04 18:41:43 +0200

    Change BaseIntegrationTest field accessor.

--- Backend/Car-Mender.API.IntegrationTests/BaseIntegrationTest.cs
+++ Backend/Car-Mender.API.IntegrationTests/BaseIntegrationTest.cs
@@ -11,35 +11,23 @@ public abstract class BaseIntegrationTest
 	private readonly AppDbContext _dbContext;
 	private readonly IServiceScope _scope;
 	protected readonly HttpClient Client;
-	private readonly TestServer _server;
+	protected readonly TestServer Server;
 	private bool _disposed;
 
 	protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
 	{
-		_server = factory.Server;
-		_scope = _server.Services.CreateScope();
-		Client = _server.CreateClient();
+		Server = factory.Server;
+		_scope = Server.Services.CreateScope();
+		Client = Server.CreateClient();
 		_dbContext = _scope.ServiceProvider.GetRequiredService<AppDbContext>();
 		_dbContext.Database.Migrate();
 	}
 
 	public void Dispose()
 	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-
-	private void Dispose(bool disposing)
-	{
-		if (_disposed) return;
-		if (disposing)
-		{
-			_dbContext.Dispose();
-			_scope.Dispose();
-			Client.Dispose();
-			_server.Dispose();
-		}
-
-		_disposed = true;
+		_dbContext.Dispose();
+		_scope.Dispose();
+		Client.Dispose();
+		Server.Dispose();
 	}
 }
