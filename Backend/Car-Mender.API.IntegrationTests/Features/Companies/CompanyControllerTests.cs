1c716f095a9a61c5ea7084ba35fa77fce4db87d1
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-06 19:29:44 +0200

    Implement integration tests for get, update and delete operations for Company.

--- Backend/Car-Mender.API.IntegrationTests/Features/Companies/CompanyControllerTests.cs
+++ Backend/Car-Mender.API.IntegrationTests/Features/Companies/CompanyControllerTests.cs
@@ -1,12 +1,29 @@
+using System.Net;
 using System.Text;
-using System.Text.Json;
+using Car_Mender.Domain.Features.Companies.DTOs;
+using Car_Mender.Domain.Features.Companies.Entities;
 using Car_Mender.Domain.Models;
 using Car_Mender.Infrastructure.Features.Companies.Commands.CreateCompany;
+using Car_Mender.Infrastructure.Persistence.DatabaseContext;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Car_Mender.API.IntegrationTests.Features.Companies;
 
-public class CompanyControllerTests(IntegrationTestWebAppFactory factory) : BaseIntegrationTest(factory)
+public class CompanyControllerTests : BaseIntegrationTest, IDisposable
 {
+	private readonly IServiceScope _scope;
+	private readonly IAppDbContext _dbContext;
+
+	public CompanyControllerTests(IntegrationTestWebAppFactory factory) : base(factory)
+	{
+		_scope = Server.Services.CreateScope();
+		var provider = _scope.ServiceProvider;
+		_dbContext = provider.GetRequiredService<IAppDbContext>();
+	}
+
 	[Fact]
 	public async Task CreateCompanyAsync_ShouldReturnSuccessfulStatusCode()
 	{
@@ -34,6 +51,107 @@ public class CompanyControllerTests(IntegrationTestWebAppFactory factory) : Base
 		var response = await Client.PostAsync("api/company", content);
 
 		// Assert
-		Assert.True(response.IsSuccessStatusCode);
+		Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+	}
+
+	[Fact]
+	public async Task GetCompanyAsync_ShouldReturnValidCompany()
+	{
+		// Arrange
+		var company = new Company
+		{
+			Email = "test@mail.com",
+			Name = "TestName",
+			Address = new Address
+			{
+				Street = "TestStreet",
+				City = "TestCity",
+				PostalCode = "12-345",
+				Region = "TestRegion",
+				Country = "TestCountry"
+			},
+			Phone = "123456789",
+			Nip = "123-456-78-90"
+		};
+
+		await _dbContext.Companies.AddAsync(company);
+		await _dbContext.SaveChangesAsync();
+
+		// Act
+		var response = await Client.GetAsync($"api/company/{company.Id}");
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+	}
+
+	[Fact]
+	public async Task UpdateCompanyAsync_ShouldReturnSuccessfulStatusCode()
+	{
+		// Arrange
+		var company = new Company
+		{
+			Email = "test@mail.com",
+			Name = "TestName",
+			Address = new Address
+			{
+				Street = "TestStreet",
+				City = "TestCity",
+				PostalCode = "12-345",
+				Region = "TestRegion",
+				Country = "TestCountry"
+			},
+			Phone = "123456789",
+			Nip = "123-456-78-90"
+		};
+
+		await _dbContext.Companies.AddAsync(company);
+		await _dbContext.SaveChangesAsync();
+
+		var patchDoc = new JsonPatchDocument<UpdateCompanyDto>();
+		patchDoc.Replace(c => c.Name, "NewTestName");
+
+		var serializedPatchDoc = JsonConvert.SerializeObject(patchDoc);
+		var content = new StringContent(serializedPatchDoc, Encoding.UTF8, "application/json");
+		var response = await Client.PatchAsync($"api/company/{company.Id}", content);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+	}
+
+	[Fact]
+	public async Task DeleteCompanyAsync_ShouldReturnNoContentStatusCode()
+	{
+		// Arrange
+		var company = new Company
+		{
+			Email = "test@mail.com",
+			Name = "TestName",
+			Address = new Address
+			{
+				Street = "TestStreet",
+				City = "TestCity",
+				PostalCode = "12-345",
+				Region = "TestRegion",
+				Country = "TestCountry"
+			},
+			Phone = "123456789",
+			Nip = "123-456-78-90"
+		};
+
+		await _dbContext.Companies.AddAsync(company);
+		await _dbContext.SaveChangesAsync();
+
+		// Act
+		var response = await Client.DeleteAsync($"api/company/{company.Id}");
+
+		// Assert
+		Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+	}
+
+	public new void Dispose()
+	{
+		_scope.Dispose();
+		_dbContext.Dispose();
+		base.Dispose();
 	}
 }
