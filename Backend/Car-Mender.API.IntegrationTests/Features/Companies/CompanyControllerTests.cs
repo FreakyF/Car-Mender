25211cf07945fb662921cbbdf23f4f89b18aae47
Kamil Fudala <kamil.fudala44@gmail.com>
2024-07-31 18:59:04 +0200

    Implement negative tests for CompanyController.

--- Backend/Car-Mender.API.IntegrationTests/Features/Companies/CompanyControllerTests.cs
+++ Backend/Car-Mender.API.IntegrationTests/Features/Companies/CompanyControllerTests.cs
@@ -13,144 +13,274 @@ namespace Car_Mender.API.IntegrationTests.Features.Companies;
 
 public class CompanyControllerTests : BaseIntegrationTest, IDisposable
 {
-	private readonly IServiceScope _scope;
-	private readonly IAppDbContext _dbContext;
+    private readonly IServiceScope _scope;
+    private readonly IAppDbContext _dbContext;
 
-	public CompanyControllerTests(IntegrationTestWebAppFactory factory) : base(factory)
-	{
-		_scope = Server.Services.CreateScope();
-		var provider = _scope.ServiceProvider;
-		_dbContext = provider.GetRequiredService<IAppDbContext>();
-	}
+    public CompanyControllerTests(IntegrationTestWebAppFactory factory) : base(factory)
+    {
+        _scope = Server.Services.CreateScope();
+        var provider = _scope.ServiceProvider;
+        _dbContext = provider.GetRequiredService<IAppDbContext>();
+    }
 
-	[Fact]
-	public async Task CreateCompanyAsync_ShouldReturnSuccessfulStatusCode()
-	{
-		// Arrange
-		var command = new CreateCompanyCommand
-		{
-			Email = "test@mail.com",
-			Name = "TestName",
-			Address = new Address
-			{
-				Street = "TestStreet",
-				City = "TestCity",
-				PostalCode = "12-345",
-				Region = "TestRegion",
-				Country = "TestCountry"
-			},
-			Phone = "123456789",
-			Nip = "123-456-78-90"
-		};
+    [Fact]
+    public async Task CreateCompanyAsync_ValidCompany_ShouldReturnSuccessfulStatusCode()
+    {
+        // Arrange
+        var command = new CreateCompanyCommand
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
 
-		var json = JsonSerializer.Serialize(command);
-		var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var json = JsonSerializer.Serialize(command);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-		// Act
-		var response = await Client.PostAsync("api/company", content);
+        // Act
+        var response = await Client.PostAsync("api/company", content);
 
-		// Assert
-		Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-	}
+        // Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
 
-	[Fact]
-	public async Task GetCompanyAsync_ShouldReturnValidCompany()
-	{
-		// Arrange
-		var company = new Company
-		{
-			Email = "test@mail.com",
-			Name = "TestName",
-			Address = new Address
-			{
-				Street = "TestStreet",
-				City = "TestCity",
-				PostalCode = "12-345",
-				Region = "TestRegion",
-				Country = "TestCountry"
-			},
-			Phone = "123456789",
-			Nip = "123-456-78-90"
-		};
+    [Fact]
+    public async Task CreateCompanyAsync_InvalidCompany_ShouldReturnBadRequestStatusCode()
+    {
+        // Arrange
+        var command = new CreateCompanyCommand
+        {
+            Email = "invalid mail",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "123",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123",
+            Nip = "123"
+        };
 
-		await _dbContext.Companies.AddAsync(company);
-		await _dbContext.SaveChangesAsync();
+        var json = JsonSerializer.Serialize(command);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-		// Act
-		var response = await Client.GetAsync($"api/company/{company.Id}");
+        // Act
+        var response = await Client.PostAsync("api/company", content);
 
-		// Assert
-		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-	}
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
 
-	[Fact]
-	public async Task UpdateCompanyAsync_ShouldReturnSuccessfulStatusCode()
-	{
-		// Arrange
-		var company = new Company
-		{
-			Email = "test@mail.com",
-			Name = "TestName",
-			Address = new Address
-			{
-				Street = "TestStreet",
-				City = "TestCity",
-				PostalCode = "12-345",
-				Region = "TestRegion",
-				Country = "TestCountry"
-			},
-			Phone = "123456789",
-			Nip = "123-456-78-90"
-		};
+    [Fact]
+    public async Task GetCompanyAsync_ValidId_ShouldReturnValidCompany()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
 
-		await _dbContext.Companies.AddAsync(company);
-		await _dbContext.SaveChangesAsync();
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
 
-		var patchDoc = new JsonPatchDocument<UpdateCompanyDto>();
-		patchDoc.Replace(c => c.Name, "NewTestName");
+        // Act
+        var response = await Client.GetAsync($"api/company/{company.Id}");
 
-		var serializedPatchDoc = JsonConvert.SerializeObject(patchDoc);
-		var content = new StringContent(serializedPatchDoc, Encoding.UTF8, "application/json");
-		var response = await Client.PatchAsync($"api/company/{company.Id}", content);
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
 
-		// Assert
-		Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-	}
+    [Fact]
+    public async Task GetCompanyAsync_InvalidId_ShouldReturnNotFoundStatusCode()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
 
-	[Fact]
-	public async Task DeleteCompanyAsync_ShouldReturnNoContentStatusCode()
-	{
-		// Arrange
-		var company = new Company
-		{
-			Email = "test@mail.com",
-			Name = "TestName",
-			Address = new Address
-			{
-				Street = "TestStreet",
-				City = "TestCity",
-				PostalCode = "12-345",
-				Region = "TestRegion",
-				Country = "TestCountry"
-			},
-			Phone = "123456789",
-			Nip = "123-456-78-90"
-		};
+        const string invalidId = "9c0287a9-4bdc-4937-a22b-c9d1c7118803";
 
-		await _dbContext.Companies.AddAsync(company);
-		await _dbContext.SaveChangesAsync();
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
 
-		// Act
-		var response = await Client.DeleteAsync($"api/company/{company.Id}");
+        // Act
+        var response = await Client.GetAsync($"api/company/{invalidId}");
 
-		// Assert
-		Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-	}
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
 
-	public new void Dispose()
-	{
-		_scope.Dispose();
-		_dbContext.Dispose();
-		base.Dispose();
-	}
+    [Fact]
+    public async Task UpdateCompanyAsync_ValidId_ShouldReturnSuccessfulStatusCode()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
+
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
+
+        var patchDoc = new JsonPatchDocument<UpdateCompanyDto>();
+        patchDoc.Replace(c => c.Name, "NewTestName");
+
+        var serializedPatchDoc = JsonConvert.SerializeObject(patchDoc);
+        var content = new StringContent(serializedPatchDoc, Encoding.UTF8, "application/json");
+        var response = await Client.PatchAsync($"api/company/{company.Id}", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateCompanyAsync_InvalidId_ShouldReturnSuccessfulStatusCode()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
+
+        const string invalidId = "9c0287a9-4bdc-4937-a22b-c9d1c7118803";
+
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
+
+        var patchDoc = new JsonPatchDocument<UpdateCompanyDto>();
+        patchDoc.Replace(c => c.Name, "NewTestName");
+
+        var serializedPatchDoc = JsonConvert.SerializeObject(patchDoc);
+        var content = new StringContent(serializedPatchDoc, Encoding.UTF8, "application/json");
+        var response = await Client.PatchAsync($"api/company/{invalidId}", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteCompanyAsync_ValidId_ShouldReturnNoContentStatusCode()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
+
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
+
+        // Act
+        var response = await Client.DeleteAsync($"api/company/{company.Id}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task DeleteCompanyAsync_InvalidId_ShouldReturnNoContentStatusCode()
+    {
+        // Arrange
+        var company = new Company
+        {
+            Email = "test@mail.com",
+            Name = "TestName",
+            Address = new Address
+            {
+                Street = "TestStreet",
+                City = "TestCity",
+                PostalCode = "12-345",
+                Region = "TestRegion",
+                Country = "TestCountry"
+            },
+            Phone = "123456789",
+            Nip = "123-456-78-90"
+        };
+        
+        const string invalidId = "9c0287a9-4bdc-4937-a22b-c9d1c7118803";
+
+        await _dbContext.Companies.AddAsync(company);
+        await _dbContext.SaveChangesAsync();
+
+        // Act
+        var response = await Client.DeleteAsync($"api/company/{invalidId}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    public new void Dispose()
+    {
+        _scope.Dispose();
+        _dbContext.Dispose();
+        base.Dispose();
+    }
 }
