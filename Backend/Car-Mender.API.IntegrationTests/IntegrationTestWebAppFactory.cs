8a7561cbb5a8cb74c8df2fac6a1784fa27237ad6
Kamil Fudala <kamil.fudala44@gmail.com>
2024-08-01 14:43:59 +0200

    Replace test containers with usage of real database.

--- Backend/Car-Mender.API.IntegrationTests/IntegrationTestWebAppFactory.cs
+++ Backend/Car-Mender.API.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -1,39 +1,18 @@
-using Car_Mender.Infrastructure.Persistence.DatabaseContext;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using Testcontainers.MsSql;
 
 namespace Car_Mender.API.IntegrationTests;
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
 {
-	private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
-		.WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-		.WithPassword("Strong_password123!")
-		.Build();
-	public Task InitializeAsync()
-	{
-		return _dbContainer.StartAsync();
-	}
-
-	public new Task DisposeAsync()
-	{
-		return _dbContainer.StopAsync();
-	}
-
-	protected override void ConfigureWebHost(IWebHostBuilder builder)
-	{
-		builder.ConfigureTestServices(services =>
-		{
-			var descriptorType = typeof(DbContextOptions<AppDbContext>);
-
-			var descriptor = services.SingleOrDefault(s => s.ServiceType == descriptorType);
-
-			if (descriptor is not null) services.Remove(descriptor);
-
-			services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_dbContainer.GetConnectionString()));
-		});
-	}
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configBuilder =>
+        {
+            configBuilder
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables();
+        });
+    }
 }
